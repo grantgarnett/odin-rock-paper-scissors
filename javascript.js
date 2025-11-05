e2d49a3c6708476ea22cdb41c1d45404125a04bc
Grant Garnett <grantgarnett01@outlook.com>
2025-11-05 17:14:11 -0600

    Write the logic to play the entire game

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice () {
     let computerChoice = Math.floor(Math.random()*3);
     
@@ -22,9 +19,6 @@ function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toUpperCase();
     let youWon = `You won! ${humanChoice} beats ${computerChoice}`;
     let youLost = `You lost! ${computerChoice} beats ${humanChoice}`;
-
-    console.log(humanChoice);
-    console.log(computerChoice);
 
     if(humanChoice === computerChoice) {
         console.log("You tied! Try again.");
@@ -61,4 +55,57 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-playRound(getHumanChoice(), getComputerChoice());
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toUpperCase();
+        let youWon = `You won! ${humanChoice} beats ${computerChoice}`;
+        let youLost = `You lost! ${computerChoice} beats ${humanChoice}`;
+
+        if(humanChoice === computerChoice) {
+            console.log("You tied! Try again.");
+        }
+        else if (humanChoice === "ROCK") {
+            if (computerChoice === "PAPER") {
+                console.log(youLost);
+                computerScore += 1;
+            }
+            else {
+                console.log(youWon);
+                humanScore += 1;
+            }
+        }
+        else if (humanChoice === "PAPER") {
+            if (computerChoice === "ROCK") {
+                console.log(youWon);
+                humanScore += 1;
+            }
+            else {
+                console.log(youLost);
+                computerScore += 1;
+            }
+        }
+        else {
+            if (computerChoice === "ROCK") {
+                console.log(youLost);
+                computerScore += 1;
+            }
+            else {
+                console.log(youWon);
+                humanScore += 1;
+            }
+        }
+    }
+
+    for(let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    console.log(`Final Score:
+        You - ${humanScore}
+        Computer - ${computerScore}`);
+}
+
+playGame();
