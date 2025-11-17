7167f37d6f660c84c0172be1ec9706d6f89dc020
Grant Garnett <grantgarnett01@outlook.com>
2025-11-17 16:44:23 -0600

    Remove playGame and getHumanChoice functions

--- javascript.js
+++ javascript.js
@@ -10,56 +10,45 @@ function getComputerChoice () {
     }
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Rock, paper, or scissors?");
-    return humanChoice;
-}
+let humanScore = 0;
+let computerScore = 0;
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toUpperCase();
+    let youWon = `You won! ${humanChoice} beats ${computerChoice}`;
+    let youLost = `You lost! ${computerChoice} beats ${humanChoice}`;
 
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.toUpperCase();
-        let youWon = `You won! ${humanChoice} beats ${computerChoice}`;
-        let youLost = `You lost! ${computerChoice} beats ${humanChoice}`;
-
-        if(humanChoice === computerChoice) {
-            console.log("You tied! Try again.");
-        }
-        else if (humanChoice === "ROCK") {
-            if (computerChoice === "PAPER") {
-                console.log(youLost);
-                computerScore += 1;
-            }
-            else {
-                console.log(youWon);
-                humanScore += 1;
-            }
-        }
-        else if (humanChoice === "PAPER") {
-            if (computerChoice === "ROCK") {
-                console.log(youWon);
-                humanScore += 1;
-            }
-            else {
-                console.log(youLost);
-                computerScore += 1;
-            }
+    if(humanChoice === computerChoice) {
+        console.log("You tied! Try again.");
+    }
+    else if (humanChoice === "ROCK") {
+        if (computerChoice === "PAPER") {
+            console.log(youLost);
+            computerScore += 1;
         }
         else {
-            if (computerChoice === "ROCK") {
-                console.log(youLost);
-                computerScore += 1;
-            }
-            else {
-                console.log(youWon);
-                humanScore += 1;
-            }
+            console.log(youWon);
+            humanScore += 1;
         }
     }
-
-    console.log(`Final Score:
-        You - ${humanScore}
-        Computer - ${computerScore}`);
+    else if (humanChoice === "PAPER") {
+        if (computerChoice === "ROCK") {
+            console.log(youWon);
+            humanScore += 1;
+        }
+        else {
+            console.log(youLost);
+            computerScore += 1;
+        }
+    }
+    else {
+        if (computerChoice === "ROCK") {
+            console.log(youLost);
+            computerScore += 1;
+        }
+        else {
+            console.log(youWon);
+            humanScore += 1;
+        }
+    }
 }
