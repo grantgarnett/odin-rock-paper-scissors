be1706f8b7914a5853a99d540e7f637f752ba1ec
Grant Garnett <grantgarnett01@outlook.com>
2025-11-17 10:09:32 -0600

    Remove redundant function declaration

--- javascript.js
+++ javascript.js
@@ -15,46 +15,6 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toUpperCase();
-    let youWon = `You won! ${humanChoice} beats ${computerChoice}`;
-    let youLost = `You lost! ${computerChoice} beats ${humanChoice}`;
-
-    if(humanChoice === computerChoice) {
-        console.log("You tied! Try again.");
-    }
-    else if (humanChoice === "ROCK") {
-        if (computerChoice === "PAPER") {
-            console.log(youLost);
-            computerScore += 1;
-        }
-        else {
-            console.log(youWon);
-            humanScore += 1;
-        }
-    }
-    else if (humanChoice === "PAPER") {
-        if (computerChoice === "ROCK") {
-            console.log(youWon);
-            humanScore += 1;
-        }
-        else {
-            console.log(youLost);
-            computerScore += 1;
-        }
-    }
-    else {
-        if (computerChoice === "ROCK") {
-            console.log(youLost);
-            computerScore += 1;
-        }
-        else {
-            console.log(youWon);
-            humanScore += 1;
-        }
-    }
-}
-
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
