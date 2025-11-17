173af9d224f2b4cbf2f48fa87f82258882dc1055
Grant Garnett <grantgarnett01@outlook.com>
2025-11-17 17:29:05 -0600

    Update playRound function to implement in UI

--- javascript.js
+++ javascript.js
@@ -1,57 +1,81 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const heroText = document.querySelector("#hero");
+const hScoreDisplay = document.querySelector("#player-score");
+const cScoreDisplay = document.querySelector("#computer-score");
+
+const rock = document.querySelector(".rock");
+rock.addEventListener("click", playRound);
+
+const paper = document.querySelector(".paper");
+paper.addEventListener("click", playRound);
+
+const scissors = document.querySelector(".scissors");
+scissors.addEventListener("click", playRound)
+
 function getComputerChoice () {
     const computerChoice = Math.floor(Math.random()*3);
     const choiceImage = document.querySelector(".computer-image")
     
     if (computerChoice === 0) {
         choiceImage.src = "./images/rock.png";
+        return "ROCK";
     } else if (computerChoice === 1) {
         choiceImage.src = "./images/paper.png";
+        return "PAPER";
     } else {
         choiceImage.src = "./images/scissors.png";
+        return "SCISSORS";
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toUpperCase();
+function playRound(event) {
+    const computerChoice = getComputerChoice();
+    const humanChoice = event.srcElement.className.toUpperCase();
+
+    console.log(humanChoice);
     let youWon = `You won! ${humanChoice} beats ${computerChoice}`;
     let youLost = `You lost! ${computerChoice} beats ${humanChoice}`;
 
     if(humanChoice === computerChoice) {
-        console.log("You tied! Try again.");
+        heroText.textContent = "You tied! Try again.";
     }
     else if (humanChoice === "ROCK") {
         if (computerChoice === "PAPER") {
-            console.log(youLost);
+            heroText.textContent = youLost;
             computerScore += 1;
+            cScoreDisplay.textContent = computerScore;
         }
         else {
-            console.log(youWon);
+            heroText.textContent = youWon;
             humanScore += 1;
+            hScoreDisplay.textContent = humanScore;
         }
     }
     else if (humanChoice === "PAPER") {
         if (computerChoice === "ROCK") {
-            console.log(youWon);
+            heroText.textContent = youWon;
             humanScore += 1;
+            hScoreDisplay.textContent = humanScore;
         }
         else {
-            console.log(youLost);
+            heroText.textContent = youLost;
             computerScore += 1;
+            cScoreDisplay.textContent = computerScore;
         }
     }
     else {
         if (computerChoice === "ROCK") {
-            console.log(youLost);
+            heroText.textContent = youLost;
             computerScore += 1;
+            cScoreDisplay.textContent = computerScore;
         }
         else {
-            console.log(youWon);
+            heroText.textContent = youWon;
             humanScore += 1;
+            hScoreDisplay.textContent = humanScore;
         }
     }
 }
 
-getComputerChoice();
